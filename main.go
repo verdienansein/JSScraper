33c7087fa788b46ee788f6aada91a3080445f3e8
verdienansein <gugli90@hotmail.com>
2020-03-31 00:58:55 +0200

    fixed getjavascripts function

--- main.go
+++ main.go
@@ -16,20 +16,29 @@ func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
 		url := sc.Text()
-		resp, err := client.Get(url)
+		jsList := getJavascriptsFromUrl(url)
+		fmt.Println(jsList)
+	}
+}
+
+func getJavascriptsFromUrl(url string) []string {
+	var jsList []string
+	resp, err := client.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			log.Fatal(err)
 		}
-		defer resp.Body.Close()
-		if resp.StatusCode == http.StatusOK {
-			bodyBytes, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				log.Fatal(err)
-			}
-			bodyString := string(bodyBytes)
-			re := regexp.MustCompile(`(http.*\.js)`)
-			match := re.FindStringSubmatch(bodyString)
-			fmt.Println(match[1])
+		bodyString := string(bodyBytes)
+		re := regexp.MustCompile(`(https?://\S*?\.js)`)
+		match := re.FindAllStringSubmatch(bodyString, -1)
+		for _, element := range match {
+			jsList = append(jsList, element[1])
 		}
 	}
+	return jsList
 }
